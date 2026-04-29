2d55daf7e3ecc03e6f4e5d41cb849b2ec08effdd
Jorge Arce <jorgearcedev1@gmail.com>
2026-04-28 17:00:56 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import styles from './App.module.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className={styles.title}>Clima App</h1>
+      <div className={styles.container}>
+        <div>Content 1</div>
+        <div>Content 2</div>
+      </div>
+    </>
+  )
+}
+
+export default App
